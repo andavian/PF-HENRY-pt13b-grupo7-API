20eb52d2a1f2d8017a2a71fab932e5b254bdb2db
andavian <ing.elbaum@gmail.com>
2023-10-15 10:39:30 -0300

    creacion controller orders

---
+++ Api/src/routes/routes-orders.js
@@ -0,0 +1,28 @@
+//aqui van las rutas get/post/put/delete correspondientes
+const { Router } = require("express");
+const ordersRutes = Router();
+
+const {
+  getOrders,
+} = require("../controllers/orders-controllers/orderControllers");
+
+// ordersRutes.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const productById = await getOrdersById(id);
+//     res.status(200).json(productById);
+//   } catch (error) {
+//     res.status(400).json({ error: `No existe producto con el id: ${id}` });
+//   }
+// });
+
+ordersRutes.get("/", async (req, res) => {
+  try {
+    const allOrders = await getOrders();
+    return res.status(200).json(allOrders);
+  } catch (error) {
+    res.status(400).json({ message: "No hay ordenes para mostrar" });
+  }
+});
+
+module.exports = ordersRutes;
